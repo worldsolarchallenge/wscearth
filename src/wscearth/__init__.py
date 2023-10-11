83bab58185ab2bf398dcce0d494906f8639736d4
David Snowdon <3860429+dcasnowdon@users.noreply.github.com>
2023-10-11 22:31:10 +1100

    Play with ProxyFix

--- src/wscearth/__init__.py
+++ src/wscearth/__init__.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_caching import Cache
 from flask_cors import CORS
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 config = {
     "DEBUG": True,          # some Flask specific configs
@@ -10,9 +11,12 @@ config = {
     "CACHE_DEFAULT_TIMEOUT": 300
 }
 app = Flask(__name__)
+
 app.config.from_mapping(config)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1)
 cache = Cache(app)
 cors = CORS(app)
+
 
 import wscearth.views # pylint: disable=wrong-import-position
 
