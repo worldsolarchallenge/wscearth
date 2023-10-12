ccef9bbcf7b516f7b13a6dbd07b505cbd92b0df3
Gwilyn <gwilyn@karmabunny.com.au>
2023-10-12 13:05:12 +1030

    Fix list/json bug on empty query results.
    It's only likely to happen if there's absolutely no data for 'today'.

--- src/wscearth/views.py
+++ src/wscearth/views.py
@@ -99,20 +99,18 @@ GROUP BY shortname"""
     #for _,row in df.iterrows():
     #    print(row)
 
-    if len(df) == 0:
-        df = []
-        center = {
-            "lat": -25.0,
-            "lng": 130.0,
-        }
-    else:
+    items = []
+    center = {
+        "lat": -25.0,
+        "lng": 130.0,
+    }
+
+    if len(df) > 0:
+        items = json.loads(df.to_json(orient="records"))
         center = {
             "lat": df["latitude"].mean(),
             "lng": df["longitude"].mean(),
         }
-
-    # We're in 2023 and this the best Python can do?
-    items = json.loads(df.to_json(orient="records"))
 
     return json.dumps({
         "center": center,
