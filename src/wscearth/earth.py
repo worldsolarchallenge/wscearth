5809fc68ac6bfc0ae57cdfaf40364b8c26113b92
David Snowdon <3860429+dcasnowdon@users.noreply.github.com>
2023-10-15 23:28:56 +0930

    Ran everything through black.

--- src/wscearth/earth.py
+++ src/wscearth/earth.py
@@ -12,17 +12,20 @@ import simplekml
 import wscearth.influx
 
 # Circular import recommended here: https://flask.palletsprojects.com/en/3.0.x/patterns/packages/
-from wscearth import app,cache # pylint: disable=cyclic-import
+from wscearth import app, cache  # pylint: disable=cyclic-import
 
 logger = logging.getLogger(__name__)
 
 # Create an InfluxDB client object. It may make sense to have the app own this.
-client = InfluxDBClient3(host=app.config["INFLUX_URL"],
-                         token=app.config["INFLUX_TOKEN"],
-                         org=app.config["INFLUX_ORG"],
-                         database=app.config["INFLUX_BUCKET"])
+client = InfluxDBClient3(
+    host=app.config["INFLUX_URL"],
+    token=app.config["INFLUX_TOKEN"],
+    org=app.config["INFLUX_ORG"],
+    database=app.config["INFLUX_BUCKET"],
+)
 
 wsc_influx = wscearth.influx.WSCInflux(client)
+
 
 @app.route("/earth/latest.kml")
 @cache.cached(timeout=30)
@@ -40,30 +43,30 @@ def latestkml():
 
     # FIXME: Should some of this come from Karma Bunny? # pylint: disable=fixme
     icons = {
-        "Challenger":{
+        "Challenger": {
             "href": "http://maps.google.com/mapfiles/kml/paddle/purple-circle.png",
             "scale": 2.0,
-            "hotspot": (0.5,0)
+            "hotspot": (0.5, 0),
         },
-        "Cruiser":{
+        "Cruiser": {
             "href": "http://maps.google.com/mapfiles/kml/paddle/blu-blank.png",
             "scale": 2.0,
-            "hotspot": (0.5,0)
+            "hotspot": (0.5, 0),
         },
-        "Adventure":{
+        "Adventure": {
             "href": "http://maps.google.com/mapfiles/kml/paddle/blu-stars.png",
             "scale": 2.0,
-            "hotspot": (0.5,0)
+            "hotspot": (0.5, 0),
         },
-        "Trailered":{
+        "Trailered": {
             "href": "http://maps.google.com/mapfiles/kml/paddle/grn-blank.png",
             "scale": 1.0,
-            "hotspot": (0.5,0)
+            "hotspot": (0.5, 0),
         },
-        "Official Vehicles":{
+        "Official Vehicles": {
             "href": "http://maps.google.com/mapfiles/kml/shapes/caution.png",
             "scale": 1.0,
-            "hotspot": (0,0)
+            "hotspot": (0, 0),
         },
     }
 
@@ -73,17 +76,14 @@ def latestkml():
         if "scale" in icons[name]:
             pnt.style.iconstyle.scale = icons[name]["scale"]
         if "hotspot" in icons[name]:
-            hotx,hoty = icons[name]["hotspot"]
-            pnt.style.iconstyle.hotspot = \
-                simplekml.HotSpot(
-                    x=hotx,
-                    y=hoty,
-                    xunits=simplekml.Units.fraction,
-                    yunits=simplekml.Units.fraction)
+            hotx, hoty = icons[name]["hotspot"]
+            pnt.style.iconstyle.hotspot = simplekml.HotSpot(
+                x=hotx, y=hoty, xunits=simplekml.Units.fraction, yunits=simplekml.Units.fraction
+            )
 
     folders = {}
 
-    for _,row in positions.iterrows():
+    for _, row in positions.iterrows():
         trailered = False
         carclass = row["class"]
         if trailered:
@@ -96,7 +96,7 @@ def latestkml():
 
         pnt = folders[folder_name].newpoint()
         pnt.name = f"{row['teamnum']} - {row['shortname']}"
-        pnt.coords = [(row["longitude"],row["latitude"])]
+        pnt.coords = [(row["longitude"], row["latitude"])]
 
         description = f"""\
 {f"Speed: {row['speed']:.1f} km/h" if "speed" in row else ""}
@@ -110,7 +110,8 @@ def latestkml():
         logger.critical(row)
 
     logger.debug("Outputting kml: '%s'", kml.kml())
-    return flask.Response(kml.kml(), mimetype='application/vnd.google-earth.kml+xml')
+    return flask.Response(kml.kml(), mimetype="application/vnd.google-earth.kml+xml")
+
 
 @app.route("/earth/earth.kml")
 @cache.cached()
@@ -118,14 +119,13 @@ def earthkml():
     """Implement a wrapper KML which references the above"""
     kml = simplekml.Kml(name="Bridgestone World Solar Challenge")
     netlink = kml.newnetworklink(name="Latest Positions")
-    netlink.link.href = app.url_for('latestkml', _external=True)
+    netlink.link.href = app.url_for("latestkml", _external=True)
     netlink.link.refreshmode = simplekml.RefreshMode.oninterval
     netlink.link.refreshinterval = 10.0
     netlink.visibility = 1
 
     netlink = kml.newnetworklink(name="Route")
-    netlink.link.href = app.url_for('routekml', _external=True)
+    netlink.link.href = app.url_for("routekml", _external=True)
     netlink.visibility = 1
 
-
-    return flask.Response(kml.kml(), mimetype='application/vnd.google-earth.kml+xml')
+    return flask.Response(kml.kml(), mimetype="application/vnd.google-earth.kml+xml")
--- src/wscearth/influx.py
+++ src/wscearth/influx.py
@@ -3,19 +3,23 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class WSCInflux:
     """A class for implementing useful interactions with influxdb"""
+
     def __init__(self, client):
         self.client = client
 
     def get_paths(self, measurement="telemetry", shortname=""):
         """Get the path of each car. This could be a large table."""
-        query = f'SELECT * FROM "{measurement}" WHERE shortname = \'{shortname}\' and time >= -30d'
+        query = f"SELECT * FROM \"{measurement}\" WHERE shortname = '{shortname}' and time >= -30d"
         table = self.client.query(query=query, language="influxql")
 
-        df = table.select(['time', 'latitude', 'longitude', 'altitude', 'solarEnergy']) \
-            .to_pandas() \
+        df = (
+            table.select(["time", "latitude", "longitude", "altitude", "solarEnergy"])
+            .to_pandas()
             .sort_values(by="time")
+        )
 
         logger.debug(df)
         return df
@@ -28,7 +32,7 @@ SELECT LAST(latitude),latitude,longitude,*
 FROM "{measurement}"
 WHERE
 time >= now() - 1d
-GROUP BY shortname""" # pylint: disable=duplicate-code
+GROUP BY shortname"""  # pylint: disable=duplicate-code
 
         table = self.client.query(query=query, language="influxql")
 
